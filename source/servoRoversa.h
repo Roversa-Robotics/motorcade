ee01db8834b88e78ffa9d430a6716acbc570cd92
Eric Bredder <121258349+eb8ga@users.noreply.github.com>
2025-02-23 09:22:17 -0500

    Update servoRoversa.h

--- source/servoRoversa.h
+++ source/servoRoversa.h
@@ -1,6 +1,12 @@
 #ifndef SERVOROVERSA_H
 #define SERVOROVERSA_H
 
+//servo parameters
+#define MAX_US 2300
+#define MIN_US 700
+#define STOP_US 1500
+#define FREQUENCY 50
+
 void drive (int, int);
 void forward (int, int);
 void reverse (int, int);
